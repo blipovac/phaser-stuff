b049429c0cf694a4a343e999991cac0acccc201b
Bruno Lipovac <brunolipovac@pop-os.localdomain>
2020-11-14 19:21:43 +0100

    add random float generation method

--- src/helpers/common.ts
+++ src/helpers/common.ts
@@ -2,4 +2,8 @@ export class CommonHelpers {
     public static getRandomInt(max: number, min?: number): number {
         return Math.floor((Math.floor(min) || 0) + (Math.random() * Math.floor(max)));
     }
+
+    public static getRandomFloat(min: number, max: number) {
+        return Math.random() * (max - min) + min;
+    }
 }
