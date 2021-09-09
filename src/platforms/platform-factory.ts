9d85a1f57c3cdb8da6f5d2ab49cb549e62677a35
blipovac <othermmo@gmail.com>
2021-09-09 19:38:12 +0200

    refactor platform generation so platforms start shuffling after a while

--- src/platforms/platform-factory.ts
+++ src/platforms/platform-factory.ts
@@ -1,53 +1,90 @@
 import Game from "../game";
 import { CommonHelpers } from '../helpers/common';
+import 'phaser';
+
 
 export class PlatformFactory {
     numberOfPlatforms: number = 0;
+
     newTilesQuantity: number;
+
     newTilesX: number;
     newTilesY: number;
 
+    platformRow: number = 1;
+
     updatePlatforms(game: Game) {
-        if (this.numberOfPlatforms < 20) {
-            this.numberOfPlatforms++;
+        if (this.numberOfPlatforms < 4) {
+            this.createPlatforms(game);
+        } else {
+            this.shufflePlatforms(game);
+        }
 
-            this.newTilesQuantity = CommonHelpers.getRandomInt(3, 1);
-            this.newTilesX = CommonHelpers.getRandomInt(1920, 0);
-            this.newTilesY = CommonHelpers.getRandomInt(1080 - game.EARTH_HEIGHT - 150, 0);
+    }
+    shufflePlatforms(game: Game) {
+        // grass group and ground group should have same number of elements
+        // therefore corresponding ground and grass tiles should have same indices
+        const random_number = CommonHelpers.getRandomInt(game.grassGroup.getLength());
 
-            const createdPlatforms = game.platformGroup.createMultiple([
-                {
-                    key: 'earth',
-                    frameQuantity: this.newTilesQuantity,
-                    setScale: {
-                        y: 0.5
-                    },
-                    setXY: {
-                        x: this.newTilesX,
-                        y: this.newTilesY,
-                        stepX: 200,
-                    },
-                }
-            ])
+        const randomXOffset = CommonHelpers.getRandomInt(1920, 0);
 
-            
-            game.grassGroup.createMultiple({
-                key: 'grass',
+        const grassTiles = game.platformGrassGroup.getChildren()
+        const grassTile = grassTiles[random_number];
+
+        const platformGroundTiles = game.platformGroup.getChildren();
+        const platformGroundTile = platformGroundTiles[random_number];
+
+        Phaser.Actions.SetX([grassTile], randomXOffset);
+
+        Phaser.Actions.SetX([platformGroundTile], randomXOffset);
+    }
+    createPlatforms(game: Game) {
+        this.numberOfPlatforms++;
+
+        this.newTilesQuantity = CommonHelpers.getRandomInt(3, 1);
+        this.newTilesX = CommonHelpers.getRandomInt(1920, 0);
+        this.newTilesY = 1080 - game.EARTH_HEIGHT - (150 * this.platformRow);
+
+        // increment platform row after placing a new platform so new platform is generated one row above
+        this.platformRow += 1;
+
+        // we cannot render platforms over row 6 so when we reach row 6 we reset the row counter
+        if (this.platformRow == 6) {
+            this.platformRow = 1;
+        }
+
+        const createdPlatforms = game.platformGroup.createMultiple([
+            {
+                key: 'earth',
                 frameQuantity: this.newTilesQuantity,
+                setScale: {
+                    y: 0.5
+                },
                 setXY: {
                     x: this.newTilesX,
-                    y: this.newTilesY - game.EARTH_HEIGHT / 4, // Divided by 4 because we downscaled twofold the earth rte
+                    y: this.newTilesY,
                     stepX: 200,
                 },
-                setScale: {
-                    x: 1.05
-                }
-                
-            })
-
-            for (const platform of createdPlatforms) {
-                platform.refreshBody();
             }
-        }    
+        ])
+
+
+        game.platformGrassGroup.createMultiple({
+            key: 'grass',
+            frameQuantity: this.newTilesQuantity,
+            setXY: {
+                x: this.newTilesX,
+                y: this.newTilesY - game.EARTH_HEIGHT / 4, // Divided by 4 because we downscaled twofold the earth rte
+                stepX: 200,
+            },
+            setScale: {
+                x: 1.05
+            }
+
+        })
+
+        for (const platform of createdPlatforms) {
+            platform.refreshBody();
+        }
     }
 }
