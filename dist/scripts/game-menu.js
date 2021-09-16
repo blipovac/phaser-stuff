f8da7642291192f486808485d5ea25969758fd85
blipovac <othermmo@gmail.com>
2021-09-16 21:48:36 +0200

    refactor game menu

--- dist/scripts/game-menu.js
+++ dist/scripts/game-menu.js
@@ -5,7 +5,7 @@ window.addEventListener("load", function () {
     const highScoresButton = document.getElementById("highScoresButton");
     highScoresButton.addEventListener("click", goToHighScores);
 
-    const settingsButton = document.getElementById("settingsButton");
+    const settingsButton = document.getElementById("myScoresButton");
     settingsButton.addEventListener("click", goToHighScores);
 })
 
@@ -19,5 +19,5 @@ function goToHighScores() {
 }
 
 function goToSettings() {
-    window.location.href = "settings.html";
+    window.location.href = "my-scores.html";
 }
