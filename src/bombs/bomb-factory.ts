6a838e44f7330cab5c54391679b12d18b05f950f
Bruno Lipovac <othermmo@gmail.com>
2021-12-02 11:38:30 +0100

    test ssh agent

--- src/bombs/bomb-factory.ts
+++ src/bombs/bomb-factory.ts
@@ -11,6 +11,5 @@ export class BombFactory {
         bomb.setCollideWorldBounds(true);
         bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
         bomb.allowGravity = false;
-        
     }
 }
